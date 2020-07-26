76a48f1ca234ad7d0483e633b32522a755b947a1
Ayşan Çetin <52484079+AysanCetin@users.noreply.github.com>
2020-07-27 01:35:03 +0300

    Update models.py

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # TODOs lar ile ilgili bir sınıf oluşturup içerisinde belirli tanımlamalar yapalım...
 
 class ToDos(models.Model):
-    # ToDo muzun bir başlığı olsun:
+    # ToDo başlığı:
     title = models.CharField(max_length=100) # Başlığın max uzunluğu 100 karakter...
     description = models.TextField(max_length=1000, blank=True) # blank=True --->>> bu alanı boş bırakma özgürlüğü veriyor
     # title doldurulması zorunlu olan alan ama description öyle değil.
@@ -25,9 +25,8 @@ class ToDos(models.Model):
 
 # Bu yazdıklarımızı kaydettik ama bunun veri tabanına gitmesi için 
 # migrate de migration u yapmamız gerekiyor (todo_project içerisinde migration u yaparız.)
-# (dJangoEnv) (base) artint@artint-thinkpad:~/Desktop/n2mobil/dJangoEnv/todo_project$ python manage.py makemigrations
 
-# -1- python manage.py makemigrations --->>> -Create model ToDos --->>> todo_app içerisinde de 0001_initial.py şeklide bir dosya oluşur.
+# -1- (dJangoEnv) artint@artint-thinkpad:~/Desktop/n2mobil/dJangoEnv/todo_project$ python manage.py makemigrations
   
   # Migrations for 'todo_app':
   # todo_app/migrations/0001_initial.py
@@ -35,7 +34,7 @@ class ToDos(models.Model):
  
   # Yani biz models.py içerisinde bir kod yazdık, bu kodlar sql komutlarına çevrilir ve tablo oluşur... 
 
-# -2- python manage.py migrate --->>> yapınca da ... OK der. yani aktif hale gelmiştir...
+# -2- (dJangoEnv) artint@artint-thinkpad:~/Desktop/n2mobil/dJangoEnv/todo_project$ python manage.py migrate
   
    # Operations to perform:
      # Apply all migrations: admin, auth, contenttypes, sessions, todo_app
@@ -45,16 +44,12 @@ class ToDos(models.Model):
 # Şimdi bu todo listemiz admin sayfasında görülecek. O yüzden admin.py ye gidip:
   # from.models import ToDo ---> deyip bunu admin sayfasına kabul ettirmek için de --->>> admin.site.register(ToDo) deriz.
 
-
 # todo eklediğimizde "ToDo object (1)" şeklinde görülür. Ancak biz başlıklar görünsün istiyoruz. Bunun için:
     def __str__(self): # fonksiyonunu getirip,
         return self.title
 
 # return self.name --->>> return self.title  şeklinde değiştiririz...
 
-# database de oluşturmuş olduğumuz, todo larımız çekip Home page de gösterelim.
-# öncelikle, göstereceğimiz yer views bölümü olacak. database e kaydettiğimiz todo ları views.py bölümünde çekeriz.
-# Çektikten sonra da index.html de gösterme işlemi yaparız. 
 
 
 
