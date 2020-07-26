7c20a3110d9c110295d3b079943abffc9e59ad70
Ayşan Çetin <52484079+AysanCetin@users.noreply.github.com>
2020-07-27 01:38:20 +0300

    Update views.py

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import ToDos
 from .forms import ListForm
 
+
 def index(request):
     if request.method == "POST":
         form = ListForm(request.POST or None)
@@ -15,15 +16,12 @@ def index(request):
         todo_list = ToDos.objects.all()
         return render(request, "todo_application/index.html", {"todo_list":todo_list})
 
-
-#def index(request):
-#    # database'de kaydettiğimiz todo ları çekebilmemiz için "object" isimli bir method var. Onu kullanacaz:
-#    todo_list = ToDos.objects.all() # models.py de oluşturduğumuz ToDos class'ındaki tüm objeleri getir dyoruz
-#    return render(request, "todo_application/index.html", {"todo_list":todo_list})
-#    # database deki tüm verileri aldık. Şimdi de index.html e gidip bunu görselleştirmemiz lazım...
+    
 
 def about(request):
     return render(request, "todo_application/about.html")
+
+
 
 def create(request):
 
@@ -44,17 +42,20 @@ def delete(request, ToDos_id):
     todo.delete()
     return redirect("index")
 
+
 def yes_finish(request, ToDos_id):
     todo = ToDos.objects.get(pk=ToDos_id)
     todo.finished = False # Şu an true olan buton, butona basınca False olsun
     todo.save()
     return redirect("index")
 
+
 def no_finish(request, ToDos_id):
     todo = ToDos.objects.get(pk=ToDos_id)
     todo.finished = True # Şu an False olan buton, butona basınca True olsun
     todo.save()
     return redirect("index")
+
 
 def update(request, ToDos_id):   # Yeni bir ToDo oluşturuyo gibi form gönderimi yapacaz. "create" fonksiyonu gibi...
     if request.method == "POST":
@@ -66,22 +67,3 @@ def update(request, ToDos_id):   # Yeni bir ToDo oluşturuyo gibi form gönderim
     else:
         todo_list = ToDos.objects.all()
         return render(request, "todo_application/update.html",{"todo_list":todo_list})
-
-
-
-
-
-"""
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request):
-    return HttpResponse("This is index page!")
-
-# return edip çıktı göstermesini sağlamak için 'from django.http import HttpResponse' şeklinde HttpResponse'u import ememiz gerekyor.
-# Ayrıca todo_project in settings.py dosyasının INSTALLED_APPS kısmında 'todo_app' şeklide uygulamamızı tanıtırız...
-
-def about(request):
-    return HttpResponse("This is about page!")
-
-"""
